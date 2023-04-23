aa00d5c129a420c080d4a438b670fb9353869789
MMNabeel <124467529+MMNabeel@users.noreply.github.com>
2023-04-24 01:59:38 +0200

    Update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.set('strictQuery', true);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://Nabeel:786Pakistan1234@cluster0.cctfjtp.mongodb.net/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Nabe@cluster0.cctfjtp.mongodb.net/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
   title: String,
